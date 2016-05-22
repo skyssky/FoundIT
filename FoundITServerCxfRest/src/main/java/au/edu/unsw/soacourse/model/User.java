3006d432ef76647f9576f178cf8c5ea837c8c4d4
Zenglin Wang <skyssky@hotmail.com>
2016-05-22 11:09:48 +1000

    commit

--- FoundITServerCxfRest/src/main/java/au/edu/unsw/soacourse/model/User.java
+++ FoundITServerCxfRest/src/main/java/au/edu/unsw/soacourse/model/User.java
@@ -1,5 +1,60 @@
 package au.edu.unsw.soacourse.model;
 
-public class User {
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement
+public class User {
+	private String username;
+	private String profileId;
+	private String detail;
+	private String skill;
+	private String experience;
+	
+	public User() {
+		
+    }
+	
+	public User(String username, String profileId, String detail, String skill, String experience) {
+		this.username = username;
+		this.profileId = profileId;
+		this.detail = detail;
+		this.skill = skill;
+		this.experience = experience;
+    }
+	
+	public String getUsername() {
+        return this.username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getProfileId() {
+        return this.profileId;
+    }
+    public void setProfileId(String profileId) {
+        this.profileId = profileId;
+    }
+    
+    public String getDetail() {
+        return this.detail;
+    }
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+    
+    public String getSkill() {
+        return this.skill;
+    }
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+    
+    public String getExperience() {
+        return this.experience;
+    }
+    public void setExperience(String experience) {
+        this.experience = experience;
+    }
+    
 }
--- FoundITServerCxfRest/src/main/java/au/edu/unsw/soacourse/server/UsersResource.java
+++ FoundITServerCxfRest/src/main/java/au/edu/unsw/soacourse/server/UsersResource.java
@@ -1,11 +1,16 @@
 package au.edu.unsw.soacourse.server;
 
+import java.io.File;
+import java.io.IOException;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import au.edu.unsw.soacourse.model.User;
@@ -20,21 +25,39 @@ public class UsersResource {
  
     @GET					// the method will handle GET request method on the said path
     @Path("/profile/{input}")	// this method will handle request paths http://localhost:8080/FoundITServerCxfRest/hello/echo/{some text input here}
-    @Produces("application/xml")	// the response will contain text plain content. (Note: @Produces({MediaType.TEXT_PLAIN}) means the same)
-    public User getUserProfile(@PathParam("input") String input) {	// map the path parameter text after /echo to String input.
+    @Produces("text/plain")	// the response will contain text plain content. (Note: @Produces({MediaType.TEXT_PLAIN}) means the same)
+    public String getUserProfile(@PathParam("input") String input) {	// map the path parameter text after /echo to String input.
     	
+//    	User user = 
     	
-    	User user = new User();
-    	return user;
+//		if(user == null) throw new RuntimeException("GET: User " + user +  " is not found");
+		
+    	String userDir = System.getProperty("user.dir");
+    	String sourceFile = userDir + "/User/UserProfiles.xml";
+    	String xslFile = userDir + "/User/getUserProfile.xsl";
+    	String targetFile = userDir + "/User/" + input + ".xml";
+    	Runtime rt = Runtime.getRuntime();
+//    	try {
+//			Process pr = rt.exec("java -jar saxon9he.jar " + sourceFile + " " + xslFile + " username=" + input + " > " + targetFile);
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//    	File fXmlFile = new File("userDir/");
+    	System.out.println("GET: userDir = " + userDir);
+    	return String.valueOf(userDir);
     }
 
     @POST							// the method will handle POST request method on the said path
-    @Produces("application/json")	// the response will contain JSON
-    @Consumes("application/json")	// applies to the input parameter JsonBean input. map the POST body content (which will contain JSON) to JsonBean input
-    @Path("/jsonBean")				// this method will handle request paths http://localhost:8080/FoundITServerCxfRest/hello/jsonBean
-    public Response modifyJson(JsonBean input) {
-        input.setVal2(input.getVal1());
-        return Response.ok().entity(input).build();
+    @Produces(MediaType.APPLICATION_XML)	// the response will contain JSON
+    @Consumes(MediaType.APPLICATION_XML)	// applies to the input parameter JsonBean input. map the POST body content (which will contain JSON) to JsonBean input
+//    @Path("/jsonBean")				// this method will handle request paths http://localhost:8080/FoundITServerCxfRest/hello/jsonBean
+    public User addUserProfile(User user) {
+//        System.out.println("POST username = " + username);
+//        System.out.println("POST profileId = " + profileId);
+//        System.out.println("POST title = " + title);
+    	System.out.println("POST: user ");
+        return user;
     }
 }
 
